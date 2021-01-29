c0f288497c88af6edd003ce0f965df8487a6b72b
MirzkisD1Ex0 <lovezcz13685521730@live.com>
2021-01-29 10:00:49 +0800

    1

---
+++ Vue/networkmusicplayer/src/components/MusicList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="music-list">
+    <div class="test">
+      <audio :src="tempUrl" controls autoplay>
+        您的浏览器不支持 audio 标签。
+      </audio>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tempUrl: require("../assets/TheFatRat-Jackpot.mp3"),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.music-list {
+}
+
+.music-list audio {
+  font-size: 24px;
+  width: 300px;
+  height: 40px;
+}
+
+#test {
+  border: orange 1px solid;
+}
+</style>
